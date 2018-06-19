870ac4beb36416712c29650eed4c9a149679c606
Pieter van de Ven <pieter@dj-consultancy.nl>
2018-06-19 23:23:11 +0200

    Create tabbed dialog screen

--- www/go/modules/community/multi_instance/views/extjs3/Dialog.js
+++ www/go/modules/community/multi_instance/views/extjs3/Dialog.js
@@ -1,9 +1,12 @@
 go.modules.community.multi_instance.Dialog = Ext.extend(go.panels.TabWindow, {
 
 	title: t("Instances"),
+	stateId: 'multi_instance-Dialog',
+	entityStore: go.Stores.get("Instance"),
+	autoHeight: true,
 
 	initComponent: function () {
-		go.modules.community.multi_instance.Dialog.superclass.initComponent(this);
+		go.modules.community.multi_instance.Dialog.superclass.initComponent.call(this);
 
 		this.addPanel(go.modules.community.multi_instance.InstanceDialog);
 	}
--- www/go/modules/community/multi_instance/views/extjs3/InstanceDialog.js
+++ www/go/modules/community/multi_instance/views/extjs3/InstanceDialog.js
@@ -1,30 +1,39 @@
 go.modules.community.multi_instance.InstanceDialog = Ext.extend(Ext.form.FormPanel, {
-	stateId: 'multi_instance-InstanceDialog',
-	title: t('Instance'),
-	entityStore: go.Stores.get("Instance"),
-	autoHeight: true,
-
-	onLoad(callback, tabPanel) {
-		if (tabPanel.currentId > 0) {
-			this.hostnameField.disable()
-		}
-		this.load(tabPanel.currentId);
-	},
-
-	initFormItems: function () {
+	initComponent: function() {
 		this.hostnameField = new Ext.form.TextField({
 			name: 'hostname',
 			fieldLabel: t("Hostname"),
 			anchor: '100%',
 			required: true
 		});
-		return [{
+		Ext.apply(this, {
+			title: t('Properties'),
+			autoScroll: true,
+			iconCls: 'ic-description',
+			items: [{
 				xtype: 'fieldset',
+				defaults: {
+					width: dp(240)
+				},
 				items: [
-					this.hostnameField
+					this.hostnameField,
+					{
+						xtype: 'textfield',
+						fieldLabel: 'Description',
+						anchor: '100%',
+						name: 'description'
+					}
 				]
-			}
-		];
+			}]
+		});
+		go.modules.community.multi_instance.InstanceDialog.superclass.initComponent.call(this)
+	},
+	onLoad: function(cb, tabPanel) {
+		if (tabPanel.currentId > 0) {
+			this.hostnameField.disable();
+		}
+		var instances = tabPanel.entityStore.get([tabPanel.currentId]);
+		this.load(instances[0]);
 	}
 });
 
